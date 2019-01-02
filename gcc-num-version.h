1fa866b8613fabfa03fb37533f40cc55e5c39bb2
mike632t <mike632t>
2019-01-02 12:20:43 +0000

    Added seperate include file to define version number

---
+++ gcc-num-version.h
@@ -0,0 +1,27 @@
+/*
+ *
+ * gcc-num-version.h - Number macros.
+ *
+ * Copyright(C) 2018   MEJT
+ *
+ * This  program is free software: you can redistribute it and/or modify  it
+ * under  the  terms of the GNU General Public License as published  by  the
+ * Free  Software  Foundation, either version 3 of the License, or (at  your
+ * option) any later version.
+ *
+ * This  program  is  distributed in the hope that it will  be  useful,  but
+ * WITHOUT   ANY   WARRANTY;   without  even   the   implied   warranty   of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You  should have received a copy of the GNU General Public License  along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 02 Jan 18         - Initial version - MEJT
+ *
+ */
+
+#indef GIT_COMMIT_ID
+#define GIT_COMMIT_ID $Format:%H$
+#define GIT_COMMIT_TIME $Format:%aI$
+#endif
